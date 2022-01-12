4eec416b9dccc71f1cbd5144efa11d8e51c00a61
Aditya Ajie Nugroho <adityaajinug@gmail.com>
2022-01-13 06:53:51 +0700

    update

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'sibili');
+$autoload['helper'] = array('url', 'sibili', 'text');
 
 /*
 | -------------------------------------------------------------------
